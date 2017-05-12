dda9d0e30a417650fb9877831aa227077cad83e3
wang <wang1386528@gmail.com>
2017-05-13 01:14:00 +0800

    Update MainTest.java

--- git/src/com/git/clazz/MainTest.java
+++ git/src/com/git/clazz/MainTest.java
@@ -3,9 +3,9 @@ package com.git.clazz;
 public class MainTest {
 
 	public static void main(String[] args) {
-		new ArrayList();
+	
 		new MainTest();
-		System.out.println("zaima");
+		System.out.println("zaima123123123");
 		System.out.println("123123132");
 	}
 
